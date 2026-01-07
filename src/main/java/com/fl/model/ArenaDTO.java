d348975ddbcff2cd9fa500c47c3ae1fe46fb77f1
unknown <bin33sa@gmail.com>
2026-01-07 14:56:42 +0900

    MercenaryDTO

--- src/main/java/com/fl/model/ArenaDTO.java
+++ src/main/java/com/fl/model/ArenaDTO.java
@@ -1,5 +1,33 @@
 package com.fl.model;
 
 public class ArenaDTO {
-
+	private Long arena_code;
+    private Long stadium_code;
+    private String arena_name;
+    private String stadium_name;
+    
+	public Long getArena_code() {
+		return arena_code;
+	}
+	public void setArena_code(Long arena_code) {
+		this.arena_code = arena_code;
+	}
+	public Long getStadium_code() {
+		return stadium_code;
+	}
+	public void setStadium_code(Long stadium_code) {
+		this.stadium_code = stadium_code;
+	}
+	public String getArena_name() {
+		return arena_name;
+	}
+	public void setArena_name(String arena_name) {
+		this.arena_name = arena_name;
+	}
+	public String getStadium_name() {
+		return stadium_name;
+	}
+	public void setStadium_name(String stadium_name) {
+		this.stadium_name = stadium_name;
+	}
 }
--- src/main/java/com/fl/model/MercenaryDTO.java
+++ src/main/java/com/fl/model/MercenaryDTO.java
@@ -1,5 +1,146 @@
 package com.fl.model;
 
 public class MercenaryDTO {
+	private Long recruit_id;      
+    private Long team_code;       
+    private Long match_code;      
+    private Long member_code;     
+    private String created_at;
+    private String status;       
+    private String title;
+    private String content;
+    private int view_count;
 
+    private String team_name;
+    private String emblem_image;
+    private String region;        
+    
+    private String member_name;
+    private String member_id;
+    
+    private String match_date;
+    
+    
+	public Long getRecruit_id() {
+		return recruit_id;
+	}
+
+	public void setRecruit_id(Long recruit_id) {
+		this.recruit_id = recruit_id;
+	}
+
+	public Long getTeam_code() {
+		return team_code;
+	}
+
+	public void setTeam_code(Long team_code) {
+		this.team_code = team_code;
+	}
+
+	public Long getMatch_code() {
+		return match_code;
+	}
+
+	public void setMatch_code(Long match_code) {
+		this.match_code = match_code;
+	}
+
+	public Long getMember_code() {
+		return member_code;
+	}
+
+	public void setMember_code(Long member_code) {
+		this.member_code = member_code;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getView_count() {
+		return view_count;
+	}
+
+	public void setView_count(int view_count) {
+		this.view_count = view_count;
+	}
+
+	public String getTeam_name() {
+		return team_name;
+	}
+
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+
+	public String getEmblem_image() {
+		return emblem_image;
+	}
+
+	public void setEmblem_image(String emblem_image) {
+		this.emblem_image = emblem_image;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getMember_name() {
+		return member_name;
+	}
+
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getMatch_date() {
+		return match_date;
+	}
+
+	public void setMatch_date(String match_date) {
+		this.match_date = match_date;
+	}
+    
+    
+    
 }
